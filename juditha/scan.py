35fe6313dff6bd95835facbaee321a77c06ebfcd
Simon Wörpel <simon.woerpel@pm.me>
2024-01-08 02:12:57 +0100

    ⚡️ (scan) iterate better through text chunks

--- juditha/scan.py
+++ juditha/scan.py
@@ -4,7 +4,8 @@ Scan fulltext for known names (fuzzy)
 Not intended for very long texts
 
 Approach:
-    - iterate through every ngram from the snippet, with the longest first
+    - split text into sentences
+    - iterate through every ngram from the sentence, with the longest first
     - normalize the ngrams and lookup as key patterns to get candidate blocks
     - compare actual candidates with real text via `fuzzysearch` and find best match
 """
@@ -12,19 +13,26 @@ Approach:
 from typing import Generator, Iterable
 
 from fuzzysearch import find_near_matches
+from nltk import sent_tokenize, word_tokenize
 from nltk.util import everygrams
 
 from juditha import settings
-from juditha.clean import clean_value, normalize
+from juditha.clean import normalize
 from juditha.util import find_best
 
 
-def get_token_keys(txt: str) -> Generator[str, None, None]:
-    txt = clean_value(txt)
-    for gram in sorted(everygrams(txt.split()), key=len, reverse=True):
-        key = normalize("".join(gram))
-        if key:
-            yield key
+def get_token_keys(txt: str, stream=False) -> Generator[str, None, None]:
+    keys = set()
+    for sent in sent_tokenize(txt):
+        for gram in sorted(everygrams(word_tokenize(sent)), key=len, reverse=True):
+            key = normalize("".join(gram))
+            if key:
+                if stream:
+                    yield key
+                else:
+                    keys.add(key)
+    if not stream:
+        yield from sorted(keys, key=len, reverse=True)
 
 
 def search_candidates(
