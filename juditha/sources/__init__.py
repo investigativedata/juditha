5850672dd04a76ad3107b5f63cf1f8232e1423aa
Simon Wörpel <simon.woerpel@pm.me>
2024-01-12 08:08:53 +0100

    🔥 drop fallback source feature

--- juditha/sources/__init__.py
+++
@@ -1,5 +0,0 @@
-from juditha.sources.aleph import Aleph
-from juditha.sources.juditha import Juditha
-from juditha.sources.wikipedia import Wikipedia
-
-__all__ = ["Wikipedia", "Aleph", "Juditha"]
--- tests/test_sources.py
+++
@@ -1,9 +0,0 @@
-from juditha.sources import Aleph, Wikipedia
-
-
-def test_sources(fixtures_path):
-    aleph = Aleph(host="https://aleph.occrp.org")
-    assert aleph.lookup("Vladimir Putin") == "VLADIMIR PUTIN"
-
-    wiki = Wikipedia(url="https://de.wikipedia.org")
-    assert wiki.lookup("angela merkel") == "Angela Merkel"
